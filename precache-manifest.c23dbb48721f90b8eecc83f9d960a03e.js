0e04571ba8ff9a67e2c65f3fec980d5f98e7cbc9
lockys <locky4567@gmail.com>
2020-03-04 18:14:26 +0800

    Updates

---
+++ precache-manifest.c23dbb48721f90b8eecc83f9d960a03e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9d1ff928398651ef1952eacea767ce",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d482f4a553419f4f41d0",
+    "url": "/monsters-rolodex/static/css/main.3a05610b.chunk.css"
+  },
+  {
+    "revision": "640fa3f2e5b37ee859bf",
+    "url": "/monsters-rolodex/static/js/2.c2a893ac.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.c2a893ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d482f4a553419f4f41d0",
+    "url": "/monsters-rolodex/static/js/main.ff6ce918.chunk.js"
+  },
+  {
+    "revision": "420e781ce2f2f4d61faa",
+    "url": "/monsters-rolodex/static/js/runtime-main.9cf5dfda.js"
+  }
+]);
